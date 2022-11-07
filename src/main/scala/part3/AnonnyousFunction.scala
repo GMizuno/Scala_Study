f73f6276270cd697d9f450aed9e968a161066698
Gabriel Mizuno <gabrielmizuno@gmail.com>
2022-11-07 20:10:10 -0300

    AnonnymousFunction

---
+++ src/main/scala/part3/AnonnyousFunction.scala
@@ -0,0 +1,31 @@
+package part3
+
+object AnonnyousFunction extends App {
+
+  // Anonynous function (LAMBDA)
+  val doubler = (x: Int) => x * 2
+  val doubler2: Int => Int = x => x * 2
+
+  println(doubler(2))
+  println(doubler2(4))
+
+  // MUltiple parameters
+  val adder: (Int, Int) => Int = (a: Int, b: Int) => a + b
+
+  // no parameter
+  val justDoSomething: () => Int = () => 3
+
+  println(justDoSomething())
+  println(justDoSomething)
+
+  val stringToIny = { (str: String) => str.toString }
+
+  // Syntatic sugar
+
+  val niceIncrementer: Int => Int = _ + 1 // x => x + 1
+  val niceAdder: (Int, Int) => Int = _ + _
+
+  println(niceIncrementer(3))
+  println(niceAdder(3, 4))
+
+}
